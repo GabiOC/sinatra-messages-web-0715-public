ab6e6936e7b32af9433fd03a95b724f4383f73c5
GabiOC <oconnor.gabriele@gmail.com>
2015-08-27 13:46:17 -0400

    all done

--- bin/post_message.rb
+++ bin/post_message.rb
@@ -1,6 +1,7 @@
 # post_message.rb
 require "net/http"
 require 'uri'
+require 'pry'
 
 puts ""
 print "Who do you want to message? "
@@ -14,15 +15,14 @@ content = gets.chomp
 puts ""
 print "Sending message..."
 
-uri = URI("http://localhost:9292")
+# Post the message to the server
 
-# TODO: Post the message to the server
-# How do you submit a POST request using Ruby?
-# Maybe a library called Net::HTTP has a post method? Google.
+uri = URI.parse("http://localhost:9292")
+params = {to: to, from: from, content: content}
+response = Net::HTTP.post_form(uri, params)
+
 if response.message == "OK"
  puts "It worked :)"
 else
  puts "Oops, something went wrong :("
 end
-
-puts ""
--- db/migrate/01_create_messages.rb
+++ db/migrate/01_create_messages.rb
@@ -8,6 +8,10 @@
 class CreateMessages < ActiveRecord::Migration
   def change
     create_table :messages do |t|
+      t.text :to
+      t.text :from
+      t.text :content
+      t.timestamps null: false
     end
   end
 end
